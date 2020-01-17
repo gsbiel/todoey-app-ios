276d8ffdb5babd38e39c333a5438a9e58b9e9fa6
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-17 07:14:04 -0500

    Commit inicial, projeto vazio

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = [
+        "Find Mike",
+        "Buy Eggs",
+        "Destroy Demogorgon"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
