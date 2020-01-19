958700b73f90cf56135e62f84eafea840496794e
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-19 16:07:39 -0500

    Criando a tela de categorias de toDo items e configurando as segues e o menu de navegacao

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by user161182 on 1/19/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
